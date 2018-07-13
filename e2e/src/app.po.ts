e72ee5baae087817a38162a3f9dc5afb16ee42d1
Radomir Skrzepij <skrzepij@gmail.com>
2018-07-13 23:29:28 +0200

    refactor project - change name from ifaktura to cconverter; add missing packages - angular flex layout, material design, material icons

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('ifv-root h1')).getText();
+    return element(by.css('cc-root h1')).getText();
   }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ifv'`, async(() => {
+  it(`should have as title 'cc'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ifv');
+    expect(app.title).toEqual('cc');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ifaktura!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cconverter!');
   }));
 });
