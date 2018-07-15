2c9725246f07e44c829b50bf1b0262b9445d4d78
MattMakers <wasik.matt@gmail.com>
2018-07-15 19:40:37 +0200

    Add common packages + create currency converter component

---
+++ src/app/components/currency-converter/currency-converter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cc-currency-converter',
+  templateUrl: './currency-converter.component.html',
+  styleUrls: ['./currency-converter.component.sass']
+})
+export class CurrencyConverterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
