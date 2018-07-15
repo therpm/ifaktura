c072265590729c584f04511f0fb8b4e066703c36
MattMakers <wasik.matt@gmail.com>
2018-07-15 19:51:53 +0200

    Add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
-import {FlexLayoutModule} from '@angular/flex-layout'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
-import {AppComponent} from './app.component'
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './/app-routing.module';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        FlexLayoutModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FlexLayoutModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
