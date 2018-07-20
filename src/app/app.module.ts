572e3dcba8d149033d4f55636956d6e8bfcf0ffc
MattMakers <wasik.matt@gmail.com>
2018-07-20 21:17:30 +0200

    CR changes -tslint changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './/app-routing.module';
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
@@ -11,8 +10,8 @@ import {AppRoutingModule} from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    FlexLayoutModule,
-    AppRoutingModule
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
