b172fbdf6fb7d7838b9c356bff55918573be2a49
Radomir Skrzepij <skrzepij@gmail.com>
2018-08-10 16:16:14 +0200

    add normalize.css and new header component. Need to change markup and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { FlexLayoutModule } from '@angular/flex-layout'
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeaderComponent } from './shared/header/header.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material/button'
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FlexLayoutModule
-
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
